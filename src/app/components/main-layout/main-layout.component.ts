5a03d0145881f3d48f6ba7293a65b09af53d5ec4
Андрей <undejavue@gmail.com>
2018-12-18 14:40:53 +0300

    Customer name corrected

--- src/app/components/main-layout/main-layout.component.ts
+++ src/app/components/main-layout/main-layout.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PollutionService } from '../../services/pollution.service';
-import { BehaviorSubject } from 'rxjs';
+import { ConfigService } from '../../services/config.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -9,13 +9,23 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class MainLayoutComponent implements OnInit {
   isReady = false;
-  constructor(private dataService: PollutionService) { }
+  owner: {
+    name: string,
+    title: string,
+    site: string,
+    department: string
+  };
+
+  constructor(private dataService: PollutionService, private config: ConfigService) { }
 
   ngOnInit() {
     this.dataService.isInitialized
       .pipe()
       .subscribe(result => {
         this.isReady = result;
+        if (this.isReady) {
+          this.owner = this.config.get('owner');
+        }
       });
   }
 
